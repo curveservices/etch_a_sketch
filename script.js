eafa616ebd1554c5188d31b8b98649d0e86c34fb
Paul <p.rossiter83@gmail.noreply.github.com>
2023-01-16 18:36:20 +0000

    UI updated with a 16 * 16 grid and mouseover Listener

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
 //Create the divs using JavaScript.
 //put your grid squares inside another “container” div
 //webpage with a 16x16 grid of square divs
-const container = document.querySelector('.container');
+let container = document.querySelector('.container');
+container.style.gridTemplateColumns = 'repeat(16 , 1fr)';
+container.style.gridTemplateRows = 'repeat(16 , 1fr)';
 
-let row;
-let column;
-
-for (let j = 0; j < 16; j++){
-    row = document.createElement('div');
-    row.className = 'flex-container';
-    row.setAttribute('id','grid-item')
-    container.appendChild(row);
-
-    for (let i = 0; i<16; i++){
-        column = document.createElement('div');
-        column.className = 'flexboxes';
-        column.classList.add = 'rows'
-        column.setAttribute('id', 'grid-item')
-        column.innerText = 'a'
-        row.appendChild(column);
-    }
-}
-const settings = document.createElement('div');
-settings.classList.add('settings')
-
-container.append(settings);
+for (let i = 0; i < 256; i++) {
+  let square = document.createElement('div');
+  square.classList.add('cell');
+  container.style.backgroundColor = ('#e0e0e0');
+//change the div’s background color using JavaScript
+  square.addEventListener('mouseover', () => {
+    (square.style.backgroundColor = 'gray')
+  });
+  container.insertAdjacentElement('beforeend', square);
+};
