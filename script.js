61d9113a1c5e67a52d63b927b34cf8f87ce9c352
Paul <p.rossiter83@gmail.noreply.github.com>
2023-03-10 17:04:16 +0000

    Script tidied up

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-//HTML button to run Javascript function 'onclick' 
-// ---------------------------------------------- 
-let color = 'black'
+//HTML button to run Javascript function 'onclick'
+// ----------------------------------------------
+let color = "black";
 
 //Create the divs using JavaScript.
 //put grid squares inside “container” div
-function populateContainer(size){
-  let container = document.querySelector('.container');
-  let squares = container.querySelectorAll('div');
-  squares.forEach(div => div.remove());
-container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
-container.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+function populateContainer(size) {
+  let container = document.querySelector(".container");
+  let squares = container.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-let amount = size*size
-for (let i = 0; i < amount; i++) {
-  let square = document.createElement('div');
-  square.classList.add('cell');
-  container.style.backgroundColor = ('#e0e0e0');
-//Set up “hover” effect so the divs change color using JavaScript
-  square.addEventListener('mouseover', colorSquare);
-  container.insertAdjacentElement('beforeend', square);
-};
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.classList.add("cell");
+    container.style.backgroundColor = "#e0e0e0";
+    //Set up “hover” effect so the divs change color using JavaScript
+    square.addEventListener("mouseover", colorSquare);
+    container.insertAdjacentElement("beforeend", square);
+  }
 }
 //webpage with a 16x16 grid of square divs
 populateContainer(16);
@@ -29,25 +29,25 @@ function changeSize(input) {
   if (input >= 2 && input <= 100) {
     populateContainer(input);
   } else {
-    console.log('input error! to many or to few squares');
+    console.log("input error! to many or to few squares");
   }
 }
 
 function colorSquare() {
-  if (color === 'random'){
+  if (color === "random") {
     // random color found thanks to stack overflow!!
     this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
   } else {
     this.style.backgroundColor = color;
   }
 }
-  
+
 function changeColor(choice) {
   color = choice;
 }
 
 function resetContainer() {
-  let container = document.querySelector('.container');
-  let squares = container.querySelectorAll('div');
-  squares.forEach(div=> div.style.backgroundColor = '#e0e0e0');
+  let container = document.querySelector(".container");
+  let squares = container.querySelectorAll("div");
+  squares.forEach((div) => (div.style.backgroundColor = "#e0e0e0"));
 }
